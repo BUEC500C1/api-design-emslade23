cf9d756075138ffa9d0fccdcbbd33fa59f59419c
Elizabeth Slade <elizabethslade23@gmail.com>
2020-02-05 14:34:49 -0500

    added UI for getting airport name, getting weather, and error checking wrong airport, also running weather api, and finding airport lat and long through csv

--- app.py
+++ app.py
@@ -2,17 +2,52 @@
 
 from flask import render_template, request, Flask
 
+import csv
+import requests
+import os
+
+
+def getAirportCoordinates(airportName):
+    # gets location of airport given the name of the airport
+    with open("airports.csv", 'r') as airportData:
+        for element in csv.reader(airportData):
+            if element[3] == airportName:
+                return [element[4], element[5]]
+    # error check, if airport is not in there
+    return "error"
+
+
+def weatherInformation(latitude, longitude):
+    # using the weather api, find the weather for the airport's location
+    appKey = os.environ['app_key']
+    api_address="http://api.openweathermap.org/data/2.5/weather?lat="+str(latitude)+"&lon="+str(longitude)+"&APPID="+str(appKey)+"&units=imperial"
+    response = requests.get(api_address).json()
+    return response
+
 app = Flask(__name__)
 app.config["DEBUG"] = True 
+
 
 @app.route("/", methods=["GET"])
 def input():
     return render_template("user_input.html")
 
+
 @app.route("/", methods=["POST"])
 def form_input():
     airportName = request.form["airportName"]
-    return render_template("weather_output.html", airportName = airportName)
+    latLongArray = getAirportCoordinates(airportName)
+    if latLongArray == "error":
+        return render_template("user_input.html", errorMessage = "Error wrong airport name. It must be exact name!")
+    result = weatherInformation(latLongArray[0], latLongArray[1])
+    print(result)
+    weather = result['weather'][0]['main']
+    temp = result['main']['temp']
+    feels_like = result['main']['feels_like']
+    temp_max = result['main']['temp_max']
+    temp_min = result['main']['temp_min']
+    humidity = result['main']['humidity']
+    return render_template("weather_output.html", weather=weather, temp = temp, feels_like = feels_like, temp_max = temp_max, temp_min = temp_min, humidity = humidity, airportName = airportName)
 
 
 app.run()
